060abba0c57260cefc07d5cb6fcd60fb4d454399
Dag-Inge Aas <daginge@gmail.com>
2011-09-23 23:54:13 +0200

    Added new android native project

--- ukappstrygthjem/src/org/isfit/trygthjem/UkappstrygthjemActivity.java
+++ ukappstrygthjem/src/org/isfit/trygthjem/UkappstrygthjemActivity.java
@@ -1,13 +1,13 @@
-package com.phonegap;
+package org.isfit.trygthjem;
 
-import com.phonegap.*;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class TrygthjemActivity extends DroidGap {
+public class UkappstrygthjemActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/demo.html");
+        setContentView(R.layout.main);
     }
 }
